d1d6c5863a5d5532cf6ce88d2925841c77e5ea6d
HamzaCanDo <134140657+HamzaCanDo@users.noreply.github.com>
2024-06-04 23:33:42 +0600

    Update fileUpload.php
    with validation

--- fileUpload.php
+++ fileUpload.php
@@ -1,25 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>file Upload</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>File Upload</title>
 </head>
 <body>
 
 <pre>
-    <?php
-    print_r($_FILES);
-    // move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/abc.jpg'); //specific name
-    move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . $_FILES['photo']['name']); //original name
-    ?>
+<?php
+// Define the allowed image mime types
+$allowed = ['image/png', 'image/jpg', 'image/jpeg'];
+
+if (isset($_FILES['photo'])) {
+  // Check if the uploaded file type is allowed
+  if (!in_array($_FILES['photo']['type'], $allowed)) {
+    echo 'Only jpg, jpeg, png files are allowed';
+    exit;
+  }
+
+  // Move the uploaded file
+  $uploadResult = move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . $_FILES['photo']['name']);
+
+  if ($uploadResult) {
+    echo 'File uploaded successfully!';
+  } else {
+    echo 'Error uploading file!';
+  }
+}
+?>
 </pre>
 
 <form method="post" enctype="multipart/form-data" >
-    <input type="file" name="photo">
-    <input type="submit" name="submit" value="Submit">
+  <input type="file" name="photo">
+  <input type="submit" name="submit" value="Submit">
 
 </form>
-    
+  
 </body>
 </html>
+
